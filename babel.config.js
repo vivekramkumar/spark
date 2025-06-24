b14748219071f84265c5cc53bcce58b199d018c7
vivekramkumar <41638630+vivekramkumar@users.noreply.github.com>
2025-06-24 21:57:18 +0530

    Updated metro.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Required for expo-router
       'expo-router/babel',
-      // Reanimated plugin should be last
       'react-native-reanimated/plugin',
     ],
   };
